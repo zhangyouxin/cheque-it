041355c645ec5149ac7905e2969e389b8c824743
zhangyouxin <zhangyouxingg@126.com>
2023-04-23 22:52:55 +0800

    chore: navbar & deposit

--- src/components/CreateCheque/index.tsx
+++ src/components/CreateCheque/index.tsx
@@ -1,27 +1,35 @@
 import * as React from 'react';
-import { Box, Radio, Stack, RadioGroup } from '@chakra-ui/react';
+import { Box, Radio, Stack, RadioGroup, Button, Input, FormControl, FormLabel, Select } from '@chakra-ui/react';
 import { isEmpty } from 'lodash';
 import { BI } from '@ckb-lumos/bi';
 import { buildDepositTx } from './depositTxBuilder';
 import { getAllLiveCells, getOffChainLocks } from '../../commons/nexusTools';
 import { completeTx, sendTx } from '../../utils/txUtils';
 import { useNetwork } from '../../hooks/useNetwork';
+import { parseAddress } from '@ckb-lumos/helpers';
 
 export function CreateCheque() {
   const { blockchainConfig, networkConfig } = useNetwork();
+  const [receirverAddress, setReceiverAddress] = React.useState('');
+  const [sudtAmount, setSudtAmount] = React.useState('');
+  const [sudtArgs, setSudtArgs] = React.useState('');
   const handleCreateCheque = async () => {
+    const receiverInfo = parseAddress(receirverAddress, { config: blockchainConfig });
     const cells = await getAllLiveCells(window.ckb);
     const locks = await getOffChainLocks(window.ckb);
     console.log('cells', cells);
     console.log('locks', locks);
+    console.log('receiverInfo', receiverInfo);
+    console.log('sudtArgs', sudtArgs);
+    console.log('sudtAmount', sudtAmount);
+    
     const senderInfo = locks[0];
-    const receiverInfo = { ...locks[1], args: '0x7e5214b07ed80b201df4ff00e27611a57f3ad5ae' };
 
     const { tx, txSkeleton } = buildDepositTx({
       blockchainConfig,
       networkConfig,
-      sudtTypeInfo: '0x58bef38794236b315b7c23fd8132d7f42676228d659b291936e8c6c7ba9f064e',
-      sudtAmount: BI.from(4).mul(1e18),
+      sudtTypeInfo: sudtArgs,
+      sudtAmount: BI.from(Number(sudtAmount) * 1e8).mul(1e10),
       senderInfo: senderInfo,
       receiverInfo: receiverInfo,
       collectedCells: cells,
@@ -36,5 +44,29 @@ export function CreateCheque() {
     console.log('txHash', txHash);
   };
 
-  return <Box>CreateCheque</Box>;
+  return (
+    <Box>
+      <FormControl>
+        <FormLabel>Choose SUDT:</FormLabel>
+        <Select placeholder="Select SUDT" onChange={(e) => setSudtArgs(e.target.value)}>
+          <option value="0x58bef38794236b315b7c23fd8132d7f42676228d659b291936e8c6c7ba9f064e">TTKN</option>
+        </Select>
+        <FormLabel>SUDT Amount:</FormLabel>
+        <Input
+          type="text"
+          value={sudtAmount}
+          onChange={(e) => setSudtAmount(e.target.value)}
+          marginBottom={2}
+        />
+        <FormLabel>Receiver Address:</FormLabel>
+        <Input
+          type="text"
+          value={receirverAddress}
+          onChange={(e) => setReceiverAddress(e.target.value)}
+          marginBottom={2}
+        />
+      </FormControl>
+      <Button onClick={handleCreateCheque}> create default cheque </Button>
+    </Box>
+  );
 }
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,8 +7,8 @@ interface Props {
 
 export function Navbar({ nickName }: Props) {
   return (
-    <Box w="100%" h="60px" bg="gray.100" position="sticky" top={0}>
-      {nickName}
+    <Box w="100%" h="60px" bg="gray.100" position="sticky" top={0} lineHeight='60px' textAlign='center' fontWeight='bold'>
+      {nickName && `Hi ${nickName},`} Welcome to Cheque-it
     </Box>
   );
 }
